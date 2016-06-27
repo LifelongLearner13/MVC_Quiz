3a59c6a0dc6d9a5f51e8d875712b1f71a06f959d
Robby Kim <robert.st.kim@gmail.com>
2016-06-27 14:28:53 -0400

    model portion: started creating prototype methods and variables

--- js/app.js
+++ js/app.js
@@ -1,45 +1,84 @@
-var QUESTIONS = [
+/*------------ MODEL ------------*/
+
+var Model = function () {
+  this.questions = [
     {
-        text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 0
+      text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
+      answers: [
+        '0815',
+        '2B',
+        'BAM128',
+        'Barely'
+      ],
+      correct: 0
     },
     {
-        text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 1
+      text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
+      answers: [
+        '0815',
+        '2B',
+        'BAM128',
+        'Barely'
+      ],
+      correct: 1
     },
     {
-        text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 2
+      text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
+      answers: [
+        '0815',
+        '2B',
+        'BAM128',
+        'Barely'
+      ],
+      correct: 2
     },
     {
-        text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 3
+      text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
+      answers: [
+        '0815',
+        '2B',
+        'BAM128',
+        'Barely'
+      ],
+      correct: 3
     }
-];
+  ];
+  this.score = 0;
+  this.currentQuestion = 0;
+}
+
+Model.prototype.resetScore = function () {
+  this.score = 0;
+}
+
+Model.prototype.checkQuestion = function (input) {
+  if (input === this.questions[this.currentQuestion].correct) {
+    this.score++;
+  }
+  this.currentQuestion++;
+}
+
+
+//var increaseScore = function () {
+//  var score = parseInt(scoreElement.text(), 10);
+//  scoreElement.text(score + 1);
+//};
+
+/*------------ VIEW ------------*/
+
+var showResults = function () {
+  questionsPageElement.hide();
+  resultsPageElement.show();
+};
+
+var showQuestions = function () {
+  resultsPageElement.hide();
+  questionsPageElement.show();
+};
+
+//var resetScore = function () {
+//  scoreElement.text(0);
+//};
 
 var questionsPageElement = $('.questions-page');
 var questionCurrentElement = $('.question-current');
@@ -51,59 +90,39 @@ var resultsPageElement = $('.results-page');
 var scoreElement = $('.score');
 var restartButtonElement = $('.restart-button');
 
-var showResults = function() {
-    questionsPageElement.hide();
-    resultsPageElement.show();
+var setQuestion = function (questionIndex) {
+  var question = QUESTIONS[questionIndex]; // model
+  questionCurrentElement.text(questionIndex);
+  questionElement.text(question.text);
+  answersElement.empty();
+  for (var i = 0; i < question.answers.length; i++) {
+    var answer = question.answers[i]; // model
+    answersElement.append('<li><button type="button">' + answer + '</button></li>');
+  }
 };
 
-var showQuestions = function() {
-    resultsPageElement.hide();
-    questionsPageElement.show();
-};
+answersElement.on('click', 'button', function () {
+  var choice = $(this).parent().index();
+  var questionIndex = parseInt(questionCurrentElement.text(), 10);
+  var question = QUESTIONS[questionIndex]; // model
+  if (question.correct === choice) { // model
+    increaseScore();
+  }
 
-var resetScore = function() {
-    scoreElement.text(0);
-};
-
-var increaseScore = function() {
-    var score = parseInt(scoreElement.text(), 10);
-    scoreElement.text(score + 1);
-};
-
-var setQuestion = function(questionIndex) {
-    var question = QUESTIONS[questionIndex];
-    questionCurrentElement.text(questionIndex);
-    questionElement.text(question.text);
-    answersElement.empty();
-    for (var i=0; i<question.answers.length; i++) {
-        var answer = question.answers[i];
-        answersElement.append('<li><button type="button">' + answer + '</button></li>');
-    }
-};
-
-answersElement.on('click', 'button', function() {
-    var choice = $(this).parent().index();
-    var questionIndex = parseInt(questionCurrentElement.text(), 10);
-    var question = QUESTIONS[questionIndex];
-    if (question.correct === choice) {
-        increaseScore();
-    }
-
-    if (questionIndex + 1 < QUESTIONS.length) {
-        setQuestion(questionIndex + 1);
-    }
-    else {
-        showResults();
-    }
+  if (questionIndex + 1 < QUESTIONS.length) { // model
+    setQuestion(questionIndex + 1);
+  } else {
+    showResults();
+  }
 });
 
-restartButtonElement.click(function() {
-    setQuestion(0);
-    resetScore();
-    showQuestions();
+restartButtonElement.click(function () {
+  setQuestion(0); // mostly view
+  resetScore(); // view
+  showQuestions(); // view
 });
 
-$(document).ready(function() {
-    questionsTotalElement.text(QUESTIONS.length);
-    setQuestion(0);
+$(document).ready(function () {
+  questionsTotalElement.text(QUESTIONS.length); // view
+  setQuestion(0); // mostly view
 });
